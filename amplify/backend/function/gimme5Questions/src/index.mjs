ea0677df14e301af52c16ac1c44461c5c05b9f4a
Kristian Cabading <kcabading@gmail.com>
2023-08-09 22:00:08 +0800

    Used a secure string to store db connection details

--- amplify/backend/function/gimme5Questions/src/index.mjs
+++ amplify/backend/function/gimme5Questions/src/index.mjs
@@ -1,18 +1,39 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+
+async function getDBConnectionString() {
+    const ssmClient = new SSMClient({ region: process.env.REGION });
+    const input = { // GetParametersRequest
+        Name: process.env.DBCON_PATH,
+        WithDecryption: true,
+    };
+    const command = new GetParameterCommand(input);
+    try {
+        const data = await ssmClient.send(command);
+        return data['Parameter']['Value']
+        // process data.
+    } catch (error) {
+        // error handling.
+        console.log('SSM CLIENT ERROR', error)
+    }   
+}
+
+const dbConnectionString = await getDBConnectionString()
+const client = new MongoClient(dbConnectionString);
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 export const handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
-    
     const method = event.httpMethod
     const queryParams = event.queryStringParameters
     let response = {}
     const db = await client.db("gimme5");
     const collection = await db.collection("questions");
+    
+    console.log('COLLECTIONS', collection)
     
     switch(method){
         case 'GET':
@@ -42,11 +63,11 @@ export const handler = async (event) => {
     
     return {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-     headers: {
-         "Access-Control-Allow-Origin": "*",
-         "Access-Control-Allow-Headers": "*"
-     },
+        //  Uncomment below to enable CORS requests
+        headers: {
+             "Access-Control-Allow-Origin": "*",
+             "Access-Control-Allow-Headers": "*"
+        },
         body: JSON.stringify(response.body),
     };
 };
